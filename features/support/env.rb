7da26b6e002cbdcfa621d0736d6f355cb26d2cc4
Daniel Schmidt <daniel.schmidt@adytonsystems.com>
2014-08-08 17:03:00 +0200

    [Cucumber] Add creating of fixtures before scenarios
    Signed-off-by: Daniel Schmidt <daniel.schmidt@adytonsystems.com>

--- features/support/env.rb
+++ features/support/env.rb
@@ -10,8 +10,19 @@ FIXTURES_PATH = File.expand_path(File.join(File.dirname(__FILE__),
                              "fixtures"))
 
 Before do
+  $git_fixture_dir_exists ||= false
   @dirs = [FIXTURES_PATH]
+  ["no_git", "with_git"].each do |dir|
+    git_path = File.join(FIXTURES_PATH, dir)
+    FileUtils.mkdir_p(git_path)
+    if dir.eql?("with_git")
+      Dir.chdir(git_path)
+      `git init .`
+    end
+  end
+  $git_fixture_dir_exists = true
 end
+
 
 After do
   %w{ pre-commit
